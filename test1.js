a95943c9a39c5998f27f77b086e15ba101a565d2
Katya P <katpran@gmail.com>
2024-07-10 17:21:06 +0200

    test

---
+++ test1.js
@@ -0,0 +1,5 @@
+console.log('test1.js loaded');
+
+export function testFunction1() {
+    console.log('testFunction1 called');
+}
---
+++ test2.js
@@ -0,0 +1,5 @@
+import { testFunction1 } from './test1.js';
+
+console.log('test2.js loaded');
+
+testFunction1();

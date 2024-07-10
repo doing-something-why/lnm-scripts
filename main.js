7377a7f7c320a9f2747695029b881ae441eca158
Katya P <katpran@gmail.com>
2024-07-10 17:33:59 +0200

    s

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
+import { initCountrySelection } from './country.js';
 import { initFilters } from './filters.js';
 import { initSearchForm } from './search.js';
 import { initProductDisplay } from './productDisplay.js';
-import { initCountrySelection } from './country.js';
 
 document.addEventListener("DOMContentLoaded", function() {
+  initCountrySelection();
   initFilters();
   initSearchForm();
   initProductDisplay();
-  initCountrySelection();
 });
